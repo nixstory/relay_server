b2b90861f46f2a286725454253876e6f138de93c
TaeHeun Lee <nixstory@gmail.com>
2022-09-02 15:59:38 +0900

    COSMO 추가

--- cors-proxy.js
+++ cors-proxy.js
@@ -150,6 +150,18 @@ app.all('*', function (req, res, next) {
                         console.log(">>>>>>>>>> [body] : " + JSON.stringify(body, null, 4));
                     }
                 }).pipe(res);
+        } else if (ServiceType == "COSMO") {
+            console.log(">>>>>>>>>> [authorization] : " + authorization);
+            console.log(">>>>>>>>>> [req.body] : " + JSON.stringify(req.body));
+
+            request({ url: targetUrl, method: req.method, json: req.body, headers: { 'authorization': authorization }, },
+                function (error, response, body) {
+                    if (error) {
+                        console.error('error: ' + error)
+                    } else {
+                        console.log(">>>>>>>>>> [body] : " + JSON.stringify(body, null, 4));
+                    }
+                }).pipe(res);
         } else {
             console.log(">>>>>>>>>> [TRXCD] : " + req.body.dataHeader.trxCd);
             console.log(">>>>>>>>>> [REQUEST/HEAD] : " + JSON.stringify(req.body.dataHeader, null, 4));
